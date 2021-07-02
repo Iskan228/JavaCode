0bf5655b0582479e3db9df4252558cf08d1c612b
Iskander <56783798+Iskan228@users.noreply.github.com>
2021-07-02 21:35:00 +0300

    Types.java

---
+++ Main.java
@@ -0,0 +1,12 @@
+class Main {
+	public static void main(String[] args) {
+        System.out.println("Type: int; min =" + Integer.MIN_VALUE + "; max = " + Integer.MAX_VALUE);
+        System.out.println("Type: byte; min = " + Byte.MIN_VALUE + "; max = " + Byte.MAX_VALUE);
+        System.out.println("Type: short; min = " + Short.MIN_VALUE + "; max = " + Short.MAX_VALUE);
+        System.out.println("Type: long; min = " + Long.MIN_VALUE + "; max = " + Long.MAX_VALUE);
+        System.out.println("Type: char; min = " + (int)Character.MIN_VALUE + "; max = " + (int) Character.MAX_VALUE);
+        System.out.println("Type: float; min = " + Float.MIN_VALUE + "; max = " + Float.MAX_VALUE);
+        System.out.println("Type: double; min = " + Double.MIN_VALUE + "; max = " + Double.MAX_VALUE);
+        System.out.println("Type: boolean; min = " + false + "; max = " + true);
+	}
+}
